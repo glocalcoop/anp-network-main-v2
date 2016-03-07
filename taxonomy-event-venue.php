f0bae83e4744f344bdde00e153f9b81abb4cb3e5
Pea Lutz <pea@glocal.coop>
2016-03-07 02:52:49 -0500

    Changed taxonomy event pages to use same content partial.

--- taxonomy-event-venue.php
+++ taxonomy-event-venue.php
@@ -17,8 +17,6 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-			<ul class="event-list">
-
 			<?php if ( have_posts() ) : ?>
 
 				<?php get_template_part( 'components/archive-header/archive-header' ); ?>
@@ -33,7 +31,7 @@ get_header(); ?>
 						 * If you want to override this in a child theme, then include a file
 						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
 						 */
-						get_template_part( 'template-parts/content', 'event-taxonomy-loop' );
+						get_template_part( 'template-parts/content', 'event' );
 					?>
 
 				<?php endwhile; ?>
@@ -45,8 +43,6 @@ get_header(); ?>
 				<?php get_template_part( 'template-parts/content', 'none' ); ?>
 
 			<?php endif; ?>
-
-			</ul>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
