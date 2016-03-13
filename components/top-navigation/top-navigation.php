ea1cbb467cb94121e9d4b47313578334065a51ba
Pea Lutz <pea@glocal.coop>
2016-03-13 17:37:53 -0400

    Moved theme component stylesheets to `assets/styles` directory.

--- components/top-navigation/top-navigation.php
+++ components/top-navigation/top-navigation.php
@@ -1,6 +1,6 @@
 <nav id="site-navigation" class="main-navigation global-nav" role="navigation">
     <div class="menu-toggle-button-container">
-        <button class="menu-toggle" aria-controls="top-menu" aria-expanded="false"><?php esc_html_e( 'Top Menu', 'anp-network-main' ); ?></button>
+        <button class="menu-toggle" aria-controls="top-menu" aria-expanded="false"><?php esc_html_e( 'Menu', 'anp-network-main' ); ?></button>
     </div>
 	<?php
         wp_nav_menu( array(
