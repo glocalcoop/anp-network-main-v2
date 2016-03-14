4f32bd2c40c525ecfb3d9e6f68651b9d5ed30bbe
Pea Lutz <pea@glocal.coop>
2016-03-13 20:52:15 -0400

    Added a class `header-image` to header image.

--- components/custom-header/custom-header.php
+++ components/custom-header/custom-header.php
@@ -8,7 +8,7 @@
 			<div id="header-image" class="custom-header">
 				<div class="header-wrapper custom-header-container">
 					
-					<img src="<?php header_image(); ?>" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="">
+					<img src="<?php header_image(); ?>" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" class="header-image" alt="">
 				</div><!-- .header-wrapper -->
 			</div><!-- #header-image .custom-header -->
 		<?php } else { ?>
