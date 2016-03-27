7c6f873fe367e192b83f3e6c25d0ce73ae2bcf89
Pea Lutz <pea@glocal.coop>
2016-03-27 17:10:13 -0400

    Simplified event header partial.

--- template-parts/archive-event-header.php
+++ template-parts/archive-event-header.php
@@ -6,19 +6,7 @@
     <?php $venue_address = eo_get_venue_address( $venue_id ); ?>
     
     <h1 class="page-title">
-        <?php
-        if( eo_is_event_archive('day') )
-        //Viewing date archive
-            echo __('Events: ','anp-network-main').' '.eo_get_event_archive_date('jS F Y');
-        elseif( eo_is_event_archive('month') )
-        //Viewing month archive
-            echo __('Events: ','anp-network-main').' '.eo_get_event_archive_date('F Y');
-        elseif( eo_is_event_archive('year') )
-        //Viewing year archive
-            echo __('Events: ','anp-network-main').' '.eo_get_event_archive_date('Y');
-        else
-        _e('Events','anp-network-main');
-        ?>
+        <?php the_archive_title(); ?>
     </h1>
 
     <?php do_action ( 'anp_network_main_page_header_bottom' );?>
