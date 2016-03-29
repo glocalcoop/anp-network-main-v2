ad9e2071930550b9df6ea7eb2364fb6d1c0bd421
Pea Lutz <pea@glocal.coop>
2016-03-29 17:34:58 -0400

    Added conditional to sidebar to display `handbook` sidebar for post type `handbook` or `glossary`.

--- sidebar.php
+++ sidebar.php
@@ -10,10 +10,26 @@
 <!-- Don't show sidebar if full-width or grid layout -->
 <?php if( 'full' != hybrid_get_post_layout( get_the_id() ) && 'grid' != hybrid_get_post_layout( get_the_id() ) ) : ?>
 
-    <?php if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
-        <div id="secondary" class="widget-area sidebar" role="complementary">
-            <?php dynamic_sidebar( 'sidebar-1' ); ?>
-        </div><!-- #secondary -->
+    <?php do_action ( 'anp_network_main_sidebar_before' );?>
+
+    <?php if( 'handbook' == get_post_type() || 'glossary' == get_post_type() ) : ?>
+
+        <?php if ( is_active_sidebar( 'handbook' ) ) : ?>
+            <div id="secondary" class="widget-area sidebar" role="complementary">
+                <?php dynamic_sidebar( 'handbook' ); ?>
+            </div><!-- #secondary -->
+        <?php endif; ?>
+
+    <?php else : ?>
+
+        <?php if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
+            <div id="secondary" class="widget-area sidebar" role="complementary">
+                <?php dynamic_sidebar( 'sidebar-1' ); ?>
+            </div><!-- #secondary -->
+        <?php endif; ?>
+
     <?php endif; ?>
+
+    <?php do_action ( 'anp_network_main_sidebar_after' );?>
 
 <?php endif; ?>
