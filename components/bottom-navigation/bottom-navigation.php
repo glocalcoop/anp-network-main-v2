36158e4089514741d921712a818002c1ef74bcf0
Pea Lutz <pea@glocal.coop>
2016-04-22 23:21:35 -0400

    Set footer menu fallback to false so default menu doesn't appear when no menu is selected. Suppress footer nav row if none exists

--- components/bottom-navigation/bottom-navigation.php
+++ components/bottom-navigation/bottom-navigation.php
@@ -1,10 +1,13 @@
+<?php if( has_nav_menu( 'footer-menu' ) ) : ?>
 <nav class="bottom-navigation" role="navigation">
     <?php
         wp_nav_menu( array(
             'theme_location'    => 'footer-menu',
             'menu_class'        => 'footer-menu',
             'depth'             => 1,
-            'container_class'   => 'footer-menu-container'
+            'container_class'   => 'footer-menu-container',
+            'fallback_cb'       => false
          ) );
     ?>
 </nav><!-- .bottom-navigation -->
+<?php endif; ?>
